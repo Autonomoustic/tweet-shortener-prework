c1518fb2cf1bb8057c4226e084c1e30ae0ad3a49
ali aziz <aa_connect@outlook.com>
2018-07-23 08:50:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def dictionary
+  
+  sub_words = {
+    "hello": "hi",
+    "to, two, too": "2",
+    "be": "b",
+    "you": "u",
+    "at": "@",
+    "and": "&"
+  }
+end
+
+def word_substituter(string)
+  
+  string.each do 
+  
+end
